c24fb5022e7ef424e648fe4c10a19b48103e7e69
Charles Ferguson <gerph@gerph.org>
2024-12-30 04:21:35 +0000

    Add a simple test for mappings.
    We now have a simple test for mappings as well.

---
+++ testdata/objasm/definitions/mappings.s
@@ -0,0 +1,17 @@
+##
+# Mappings take two forms - those that are with register and those without.
+#
+
+; Without a register
+                                    ^ 0     ; Base
+Constant_0                          # 1
+Constant_1                          # 4
+Constant_5                          # 1
+
+        AREA |Example$$code|, CODE, READONLY
+
+        DCD     Constant_0
+        DCD     Constant_1
+        DCD     Constant_5
+
+        END
