e02673c3f1850215ec6d48b682fbbf7f4e813dc1
Charles Ferguson <gerph@gerph.org>
2025-03-01 16:14:58 +0000

    Initial work for the current pc.
    Resolving the current PC can only happen at assembly time.

---
+++ testdata/objasm/variables/current.s
@@ -0,0 +1,15 @@
+
+; Current address in ObjAsm
+
+        AREA    |C$$code|, CODE, READONLY
+
+; Write the value into a word
+        DCD     .
+
+; Assign to a constant
+here    *       .
+
+; Branch to the address
+        B       .
+
+        END
