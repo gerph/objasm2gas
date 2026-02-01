9fbf167eb33a517e957277ac9a5982081bd8b301
Charles Ferguson <gerph@gerph.org>
2026-02-01 21:38:42 +0000

    Update to version 1.7 for release.
    Releasing version 1.7 will include the :INDEX: handling. The :INDEX:
    handling is needed for the hourglass modules to build.

--- objasm2gas.pl
+++ objasm2gas.pl
@@ -13,7 +13,7 @@ no warnings qw(portable);
 use Getopt::Long qw(:config no_ignore_case bundling);
 
 my $toolname = 'objasm2gas';
-my $ver = '1.6';
+my $ver = '1.7';
 my $helpmsg = <<"USAGE";
 $toolname (v$ver) - Convert legacy ARM assembly syntax (used by objasm) to GNU syntax (GAS)
 
